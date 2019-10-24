81b5128468a4b377da6cf07f694cd7720d5d2850
David Silva <up201705373@fe.up.pt>
2019-10-24 22:41:06 +0100

    Fixed merge conflict

--- tp1/linklayer/linklayer.c
+++ tp1/linklayer/linklayer.c
@@ -285,44 +285,8 @@ int write_data(int fd, uint8_t *buffer, int length)
   return res;
 }
 
-<<<<<<< HEAD
 int llwrite(int fd, char * buffer, int length) {
   return  write_frame(fd, DATA, buffer, length);
-=======
-int llwrite(int fd, uint8_t * buffer, int length) {
-
-  int res;
-  // State-machine setup
-  struct transmitter_state_machine st_machine;
-	numTries = 1;
-	timedOut = 1;
-
-  while (numTries < 4) {
-    st_machine.currentState = T_STATE_START;
-    res = write_data(fd, buffer, length);
-    alarm(3);                 // activates 3 sec alarm
-    timedOut = 0;
-
-    // wait for answer
-    while (!timedOut) {
-
-      uint8_t currentByte;
-      res = read(fd, &currentByte, 1);                              // returns after a char has been read or after timer expired
-      printf("-Byte received from Receiver(0x%x)\n", currentByte);
-      tsm_process_input(&st_machine, currentByte);                   // state-machine processes the read byte
-
-      if (st_machine.currentState == T_STATE_STOP) {
-          if (st_machine.frame[2] == ((sequence_number + 1) << 7) | CONTROL_RR_BASE) {
-            alarm(0);
-            sequence_number = (sequence_number + 1) % 2;
-            return res;
-          }
-      }
-    }
-  }
-  log_debug("TRASMITER EXCEED NUM TRIES\n");
-  return -1;
->>>>>>> 86832a6c17de5852cdd93d7b3af255250202d33b
 }
 
 
